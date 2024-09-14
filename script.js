a4e49fc450ef2754a9090fe153d3fe4eb23a49ac
Ankan <ankarnaskar@outlook.com>
2024-09-15 00:58:27 +0530

    Blog Page With BootStrap 1

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+        const toggler = document.querySelector('.navbar-toggler');
+        const navbarContent = document.querySelector('#navbarSupportedContent');
+
+        toggler.addEventListener('click', function () {
+            navbarContent.classList.toggle('show');
+        });
+    });
+
